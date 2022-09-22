20ad1babbe259f8cdbdd8f00c168ddce7771935d
MakeithappenMike <omichaelaaron@gmail.com>
2022-09-21 21:27:40 -0400

    got tables setup

--- db/schema.sql
+++ db/schema.sql
@@ -4,15 +4,15 @@ CREATE DATABASE departments_db;
 USE departments_db;
 
 CREATE TABLE department (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30) 
+    department_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) 
 );
 
 CREATE TABLE role (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR(30),
-  salary DECIMAL,
-  department_id INT
+    role_id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT
 );
 
 CREATE TABLE employee (
@@ -20,5 +20,13 @@ CREATE TABLE employee (
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
-    manager_id INT,
+    manager_id INT
+);
+
+CREATE TABLE manager (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    department_id INT
 );
--- db/seed.sql
+++ db/seed.sql
@@ -1,23 +1,30 @@
-INSERT INTO department (department)
-VALUES ("Engineering"),
-       ("Finance"),
-       ("Legal"),
-       ("Sales"),
-       ("Support");
+INSERT INTO department (department_id, name)
+VALUES (001, "Engineering"),
+       (002, "Finance"),
+       (003, "Legal"),
+       (004, "Sales"),
+       (005, "Support");
 
-INSERT INTO role (id, title, salary, department_id)
-VALUES (1, "Engineer I", "$50k", 1),
-       (1, "Engineer II", "$50k", 1),
-       (1, "Engineer III", "$50k", 1),
-       (1, "Staff Engineer", "$50k", 1),
-       (1, "Principle Engineer", "$50k", 1),
-       (1, "Distinguished Engineer", "$50k", 1),
-       (1, "Chief Financial Officer", "$50k", 1),
-       (1, "Paralegal", "$50k", 1),
-       (1, "Chief Councel", "$50k", 1),
-       (1, "Support Representative", "$50k", 1),
-       (1, "Tech Support Engineer", "$50k", 1),
-       (1, "Manager", "$50k", 1);  
+INSERT INTO role (role_id, title, salary, department_id)
+VALUES (001, "Engineer I", "75000", 001),
+       (002, "Engineer II", "75000", 001),
+       (003, "Engineer III", "75000", 001),
+       (004, "Staff Engineer", "75000", 001),
+       (005, "Principle Engineer", "75000", 001),
+       (006, "Distinguished Engineer", "75000", 001),
+       (007, "Engineering Manager", "75000", 001),
+       (008, "Chief Financial Officer", "75000", 002),
+       (009, "Paralegal", "75000", 003),
+       (010, "Chief Councel", "75000", 003),
+       (011, "Sales Lead", "75000", 004),
+       (012, "Sales Associate", "75000", 004),
+       (013, "Sales Manager", "75000", 004),
+       (014, "Support Representative", "75000", 005),
+       (015, "Tech Support Engineer", "75000", 005),
+       (016, "Support Manager", "75000", 005);
 
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (1, "FirstName", "LastName", 1, 1),
+VALUES (001, "FirstName", "LastName", 001, 001);
+
+INSERT INTO manager (id, first_name, last_name, role_id, department_id)
+VALUES (001, "FirstName", "LastName", 001, 001);
